e9a40bf37faa4ac64b3b8cfc81fbb81f6d9e1e63
BenignGuardian <69319092+BenignGuardian@users.noreply.github.com>
2020-08-11 14:42:11 -0700

    Initial commit session 4

---
+++ Session 2 Terrain/Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; //
+
+public class Test : MonoBehaviour
+    // 'Public' means other scripts can find this class and access things withing the class
+    /* 'Class' means script 
+     * 'Test' refers to the name of the script in the Unity editor. 
+     * : Collon ':' indicates inherentance. What does this script inherent from it inherents from MonoBehavior 
+        'MonoBehaviour' means when to call a function (when should a specific thing happen)
+        lets the script when the functions below run. 
+     */
+
+{
+    // Start is called before the first frame update
+    // 'Void' is a return type of a function. It's not a constant way of naming
+    // 'Start' is the name of the function
+    // 'circle brackets ()' e.g. Arguments can be put into the (circle brakcets) Sum(int x, int y,) you can call this finctoin bus saying Sum2,5)
+    void Start() // a function. After the start function runs once and
+    {
+        
+    }
+
+    // Update is called once per frame. Runs after Start function for every frame
+    void Update() // a function. The Update function loops over and over again unlike the start function 
+    {
+        
+    }
+    
+}
